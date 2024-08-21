ede6d9e7027fe68564e6c4564c207fef05415336
Jonathan Cruz <>
2024-08-21 14:35:26 -0300

    Clase 04 - Funciones con parametros y valores de retorno

--- clase_04/funciones.js
+++ clase_04/funciones.js
@@ -1,5 +1,19 @@
+// Variable Global
 let siglas;
 
-function ingresar(){
+function ingresar(){ // Agregar validaciones!
     siglas = prompt('Ingrese las siglas dw, dm, dg');
+    siglas = siglas.toLowerCase();
+}
+
+function mostrar(){
+    if( siglas == 'dw'){
+        console.info('Carrera Diseño Web');
+    } else if( siglas == 'dm'){
+        console.info('Carrea Diseño Multimedial');
+    } else if( siglas === 'dg'){
+        console.info('Carrea Diseño Gráfico');
+    } else {
+        console.error('Siglas invalidas');
+    }
 }
