a3b15c3ff354569485ee4ec2fe2cd6934734e25e
Jonathan Cruz <>
2024-08-21 15:06:01 -0300

    Clase 04 - Funciones con parametros y valores de retorno

--- clase_04/funciones.js
+++ clase_04/funciones.js
@@ -17,3 +17,28 @@ function mostrar(){
         console.error('Siglas invalidas');
     }
 }
+
+const darPresente = function( nombre=null ){
+    const fecha = new Date().toLocaleDateString();
+    const hora = new Date().toLocaleTimeString();
+    if( nombre==null){
+        alert('Falta el parametro nombre')
+    } else {
+        console.log(`Se dio el Presente a ${ nombre }`);
+    }
+    // Retorna el valor de la variable Fecha
+    return fecha;
+}
+
+let presente = darPresente( 'José' );
+console.log('José ' + presente)
+//console.log(nombre ); // No funciona porque nombre es un parametro local de la función
+
+
+const sumar = function(n1, n2){
+    let resultado = n1 + n2;
+    return resultado
+}
+
+let suma = sumar(2, 4);
+console.log('El valor de la suma es ' + suma);
