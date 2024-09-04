2fd3be4b4afa1e02e94b013df4f823af63323169
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2024-09-04 15:17:53 -0300

    Clase 07 - Objetos

--- clase_07/ejercicio_template/main.js
+++ clase_07/ejercicio_template/main.js
@@ -1,3 +1,41 @@
+const alumnos = [];
+const tbody = document.querySelector('tbody');
 const cargarAlumno = () =>{
-    let nombre = prompt('Ingrese el nombre')
+    // Solicito los datos
+    const nombre = prompt('Ingrese el nombre');
+    const apellido = prompt('Ingrese el apellido');
+    let nota;
+    do {
+        nota = +prompt('Ingrese la nota');
+        if(nota < 0 || nota > 10 ) {
+            alert('Nota invalida');
+        }
+    } while( nota < 0 || nota > 10)
+
+    // Creo el array asociativo
+    const alumno = [];
+    alumno['nombre'] = nombre;
+    alumno['apellido'] = apellido;
+    alumno['nota'] =  nota;
+    // Agrego el asociativo al Array Global
+    alumnos.push(  alumno  );
+}
+
+const mostrarDatos = () =>{
+    console.table( alumnos);
+    // Limpiar el contenedor
+    tbody.innerHTML = '';
+    for( i in alumnos  ){
+        const nombre = alumnos[i]['nombre'];
+        const apellido = alumnos[i]['apellido'];
+        const nota = alumnos[i]['nota'];
+        color = nota >= 7 ? 'green' : 'red';
+        tbody.innerHTML += `<tr>
+                                <td> ${ (  parseInt(i)+1) }</td>
+                                <td>${ nombre }</td>
+                                <td>${ apellido }</td>
+                                <td><span style="background-color:${color}">${ nota }</span></td>
+                            </tr>`;
+    }
+
 }
