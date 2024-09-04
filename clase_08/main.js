c7727bdf43448f7246b243cba107253231564cea
Jonathan Cruz <jonathan.cruz@davinci.edu.ar>
2024-09-04 15:36:32 -0300

    Clase 07 - Objetos

--- clase_08/main.js
+++ clase_08/main.js
@@ -1,2 +1,30 @@
 const ul = document.querySelector('#ul-list');
 //                       0               1                2
+let nombre = 'Jorge';
+let edad = 21;
+let estudiante =  true;
+let materias = ['Maquetado', 'Programación I'];
+/* ----------------------------- Objeto Literal ----------------------------- */
+const objeto = {
+    // Atributos: Las propiedad / Variables
+    nombre: 'Rocio',
+    apellido: 'Ruiz',
+    edad: 25,
+    estudiante: false,
+    materias: [],
+    // Métodos: Acciones / funciones
+    loguear: function(){
+        console.log('Iniciando sesión');
+    },
+    saludar: function(){
+        return' Hola soy ' + this.nombre;
+    }
+}
+
+console.log( objeto.nombre );  
+objeto.loguear();
+const mensaje = objeto.saludar();
+console.info(mensaje);
+
+
+
